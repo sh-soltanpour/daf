ce8890d7ac47b3f907aefbe1e7dd5f1e4395d457
sh-soltanpour <shahryar2006@gmail.com>
2019-03-27 01:25:31 +0430

    get endrosed from server

--- src/components/SkillList/SkillList.tsx
+++ src/components/SkillList/SkillList.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SkillItem from '../SkillItem/SkillItem';
 import ProjectSkill from '../../models/ProjectSkill';
 import SkillType from '../../enums/SkillType';
@@ -8,12 +8,19 @@ export default class SkillList extends Component<Props, {}> {
     type: SkillType.endorsed
   };
 
+  private getSkillType(skill: ProjectSkill): SkillType {
+    if (this.props.type === SkillType.deletable || this.props.type === SkillType.simple)
+      return this.props.type;
+    return skill.endorsed ? SkillType.endorsed : SkillType.endorsable;
+  }
+
   render(): JSX.Element {
     return (
       <ul className="skills-list">
         {this.props.skills.map(skill => (
           <li key={skill.name}>
-            <SkillItem name={skill.name} point={skill.point} type={this.props.type} />
+            <SkillItem name={skill.name} point={skill.point}
+                       type={this.getSkillType(skill)}/>
           </li>
         ))}
       </ul>
--- src/models/ProjectSkill.ts
+++ src/models/ProjectSkill.ts
@@ -1,8 +1,10 @@
 export default class ProjectSkill{
     name: string = "";
     point: number = 0;
+    endorsed: boolean = false;
     constructor(name:string, point: number){
         this.name = name;
         this.point = point;
     }
+
 }
